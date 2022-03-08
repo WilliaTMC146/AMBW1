fd8bb156afee81c497df40b4fb3ec96b6d769b71
root <root@MSI.localdomain>
2022-03-08 15:49:59 +0800

    buat Shapes

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,18 +1,73 @@
+import 'dart:ffi';
 import 'dart:io';
 
-void shape1(int n) {
+void shape1(int t, {int j = 1}) {
   int c = 1;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (j == j / 2) {
+  int x, y, z;
+
+  for (x = 1; x <= t; x++) {
+    for (y = 1; y <= j; y++) {
+      for (z = 1; z <= t - x; z++) {
+        stdout.write(" ");
+      }
+      for (z = 1; z <= x * 2 - 1; z++) {
         stdout.write(c);
       }
+      for (z = 1; z <= t - x; z++) {
+        stdout.write(" ");
+      }
     }
+    c++;
+    print("");
+  }
+  for (x = t - 1; x >= 1; x--) {
+    for (y = j; y >= 1; y--) {
+      for (z = 1; z <= t - x; z++) {
+        stdout.write(" ");
+      }
+      for (z = 1; z <= x * 2 - 1; z++) {
+        stdout.write(c);
+      }
+      for (z = 1; z <= t - x; z++) {
+        stdout.write(" ");
+      }
+    }
+    c++;
+    print("");
   }
 }
 
-void shape2() {}
+void shape2(int t, {int j = 1}) {
+  int c = 1;
+  int x, y, z;
+
+  for (x = 1; x <= t; x++) {
+    for (y = 1; y <= j; y++) {
+      for (z = 1; z <= t - x; z++) {
+        if (c >= t) {
+          c = 1;
+        }
+        stdout.write(c);
+        c++;
+      }
+      for (z = 1; z <= x * 2 - 1; z++) {
+        stdout.write(" ");
+      }
+      for (z = 1; z <= t - x; z++) {
+        if (c >= t) {
+          c = 1;
+        }
+        stdout.write(c);
+        c++;
+      }
+    }
+    c++;
+    print("");
+  }
+}
+
 void main(List<String> arguments) {
-  //shape1(5);
-  stdout.write("clara clarissa");
+  shape1(5);
+  print("");
+  shape2(4);
 }
